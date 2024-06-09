af9e2b0ed1919f5ff94c147034bc43bb5a0e828d
jesperbrowallDV <jesper.browall@datavarde.se>
2024-06-09 15:55:36 +0200

    filter geopackage files

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-from scripts.calculate_route import calculate_route
-import geopandas as gpd
-import matplotlib.pyplot as plt
 from io import BytesIO
+from matplotlib import pyplot as plt
+from scripts.filter_geopackage import filter_geopackage
+import geopandas as gpd
 
 
 def main():
+    # Load the GeoDataFrame
 
-    # calculate_route(start_id="EC_1", end_id="AB_2")
-    gdf = gpd.read_file("kartdata/vekt_100/hojd_ln25/hojd_ln25.gpkg")
+    gdf = gpd.read_file("kartdata/vekt_100/hojd_ln25/filtered_hojd.gpkg")
 
     fig, ax = plt.subplots()
     gdf.plot(ax=ax)
 
-    # Save the plot to a BytesIO object
-    img_bytes = BytesIO()
-    plt.savefig(img_bytes, format="png")
-    plt.close(fig)
-    img_bytes.seek(0)
+    plt.savefig("test.png", format="png")
 
 
 if __name__ == "__main__":
