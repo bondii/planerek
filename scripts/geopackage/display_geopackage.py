605a224ab926312f810924cc612e1f3a622ef583
jesperbrowallDV <jesper.browall@datavarde.se>
2024-06-16 15:45:12 +0200

    filter and displaying functions

--- scripts/geopackage/display_geopackage.py
+++ scripts/geopackage/display_geopackage.py
@@ -1,6 +1,5 @@
 import geopandas as gpd
 import folium
-import webbrowser
 
 
 def convert_timestamps_to_string(df, column: str):
@@ -8,13 +7,9 @@ def convert_timestamps_to_string(df, column: str):
     return df
 
 
-def main():
+def display_geopackage(fname: str) -> None:
     gdf = gpd.read_file("kartdata/vekt_100/mark_ln25/filtered_mark.gpkg")
     gdf = convert_timestamps_to_string(gdf, "skapad")
     m = folium.Map(location=(67.2236, 17.3254))
     folium.GeoJson(gdf).add_to(m)
     m.save("mark.html")
-
-
-if __name__ == "__main__":
-    main()
